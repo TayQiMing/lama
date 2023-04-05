3cef94d349d263af9a1018c2b3a9b714c1bf38e2
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-04-05 22:15:33 +0800

    Create structure.py
    The feature extractor is typically used to extract high-level features from an image, while the gradients of the grayscale image are used to capture the edge information or the structural information of an image.
    
    The output of the feature extractor is a set of feature maps that represent the image at a higher level of abstraction.
    
    On the other hand, the gradients of the grayscale image are a mathematical way of capturing the edges and structural information of the image. The gradients represent the changes in intensity across the image, and they are a good way of identifying edges and other structural information.
    
    The feature extractor is more general-purpose and can capture a wide range of information about the image, while the gradients are more focused on capturing the structural information of the image.
    
    In general, using the feature extractor may produce better results for image processing tasks because it captures a broader range of information. However, the gradients of the grayscale image can still be effective and may be more computationally efficient.

---
+++ saicinpainting/training/losses/structure.py
@@ -0,0 +1,63 @@
+import torch.nn.functional as F
+import torch
+import torchvision.models as models
+
+class StructureLoss(torch.nn.Module):
+    def __init__(self):
+        super(StructureLoss, self).__init__()
+        self.vgg = models.vgg19(pretrained=True).features[:35].eval()
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+
+    def forward(self, predicted_img, img, mask):
+        device = predicted_img.device
+
+        # normalize
+        transform = torch.nn.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+        # preprocess images
+        predicted_img = transform(predicted_img).to(device)
+        img = transform(img).to(device)
+
+        # extract features using vgg
+        features_predicted = self.vgg(predicted_img)
+        features_img = self.vgg(img)
+
+        # calculate the structure loss using mean squared error (MSE)
+        loss = F.mse_loss(features_predicted, features_img)
+
+        # apply the mask to the loss
+        loss = torch.mean(loss * mask.to(device))
+
+        return loss
+
+
+
+
+############################### BELOW IS SAMPLE FOR USING GRADIENT OF GRAYIMAGE ###############################
+# import torch.nn.functional as F
+# import torch
+
+# class StructureLoss(torch.nn.Module):
+#     def __init__(self):
+#         super(StructureLoss, self).__init__()
+
+#     def forward(self, predicted_img, img, mask):
+#         # convert images to grayscale
+#         gray_predicted_img = torch.mean(predicted_img, dim=1)
+#         gray_img = torch.mean(img, dim=1)
+
+#         # calculate gradients
+#         gradient_x_predicted, gradient_y_predicted = torch.gradient(gray_predicted_img)
+#         gradient_x_img, gradient_y_img = torch.gradient(gray_img)
+
+#         # calculate the structure loss using mean squared error (MSE)
+#         loss = F.mse_loss(gradient_x_predicted, gradient_x_img) + F.mse_loss(gradient_y_predicted, gradient_y_img)
+
+#         # apply the mask to the loss
+#         loss = torch.mean(loss * mask)
+
+#         return loss
+
+################################################################################################################
+
