8d6f4158c3bd90d4d81b51a0bccdcb227d0719de
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-04-06 15:01:00 +0800

    Update style.py

--- saicinpainting/training/losses/style.py
+++ saicinpainting/training/losses/style.py
@@ -2,13 +2,24 @@ import torch.nn.functional as F
 import torch
 import torchvision.models as models
 
+IMAGENET_MEAN = torch.FloatTensor([0.485, 0.456, 0.406])[None, :, None, None]
+IMAGENET_STD = torch.FloatTensor([0.229, 0.224, 0.225])[None, :, None, None]
+
 class StyleLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, normalize_inputs=True):
         super(StyleLoss, self).__init__()
+        
+        self.normalize_inputs = normalize_inputs
+        self.mean_ = IMAGENET_MEAN
+        self.std_ = IMAGENET_STD
+        
         self.vgg = models.vgg19(pretrained=True).features[:35].eval()
         for param in self.vgg.parameters():
             param.requires_grad = False
 
+    def do_normalize_inputs(self, x):
+        return (x - self.mean_.to(x.device)) / self.std_.to(x.device)
+            
     def gram_matrix(self, input):
         a, b, c, d = input.size()  # a=batch size(=1)
         features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
@@ -20,10 +31,13 @@ class StyleLoss(torch.nn.Module):
         # resize images to (224, 224) and normalize  - NOTE: resize to 224 is necessary because the Gram matrix calculation requires a fixed input size
         transform = torch.nn.Sequential(
             torch.nn.Upsample(size=(224, 224), mode='bilinear', align_corners=True),
-            torch.nn.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         )
 
         # preprocess images
+        if self.normalize_inputs:
+            predicted_img = self.do_normalize_inputs(predicted_img)
+            img = self.do_normalize_inputs(img)
+
         predicted_img = transform(predicted_img).to(device)
         img = transform(img).to(device)
 
