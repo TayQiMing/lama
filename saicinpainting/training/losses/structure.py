34c2bbcb2603fec0b812c2a7f83c1ab2478abf40
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-04-06 15:56:33 +0800

    Update structure.py

--- saicinpainting/training/losses/structure.py
+++ saicinpainting/training/losses/structure.py
@@ -2,23 +2,34 @@ import torch.nn.functional as F
 import torch
 import torchvision.models as models
 
+IMAGENET_MEAN = torch.FloatTensor([0.485, 0.456, 0.406])[None, :, None, None]
+IMAGENET_STD = torch.FloatTensor([0.229, 0.224, 0.225])[None, :, None, None]
+
 class StructureLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, normalize_inputs=True):
         super(StructureLoss, self).__init__()
+        
+        self.normalize_inputs = normalize_inputs
+        self.mean_ = IMAGENET_MEAN
+        self.std_ = IMAGENET_STD
+        
         self.vgg = models.vgg19(pretrained=True).features[:35].eval()
         for param in self.vgg.parameters():
             param.requires_grad = False
 
+    def do_normalize_inputs(self, x):
+        return (x - self.mean_.to(x.device)) / self.std_.to(x.device)
+     
+            
     def forward(self, predicted_img, img, mask):
         device = predicted_img.device
         
         #no need resize to 224x224 as the vgg will resize internally
-        # normalize
-        transform = torch.nn.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-
         # preprocess images
-        predicted_img = transform(predicted_img).to(device)
-        img = transform(img).to(device)
+        if self.normalize_inputs:
+            predicted_img = self.do_normalize_inputs(predicted_img)
+            img = self.do_normalize_inputs(img)
+            
 
         # extract features using vgg
         features_predicted = self.vgg(predicted_img)
