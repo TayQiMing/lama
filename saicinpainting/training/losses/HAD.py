b4e929238d455e533476d0d094ba0c18bcf3ca9e
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-05-06 01:17:30 +0800

    Update HAD.py

--- saicinpainting/training/losses/HAD.py
+++ saicinpainting/training/losses/HAD.py
@@ -13,9 +13,16 @@ class HADLoss(torch.nn.Module):
         feat_i = discrim(predicted_img)
         feat_g = discrim(img)
 
+        # Convert the mask to a bool type
+        ori_mask = ori_mask.bool()
+        
         # Compute masked regions
         masked_i = predicted_img.masked_select(ori_mask)
         masked_g = img.masked_select(ori_mask)
+        
+        if not isinstance(supervised_mask, torch.Tensor):
+            supervised_mask = torch.tensor(supervised_mask).to(device)
+            
         masked_feat_i = feat_i.masked_select(supervised_mask)
         masked_feat_g = feat_g.masked_select(supervised_mask)
 
