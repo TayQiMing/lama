b2a575acdf151c7618d3ca48f1dfce742b7d6576
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-05-05 20:46:08 +0800

    Update total_variational.py

--- saicinpainting/training/losses/total_variational.py
+++ saicinpainting/training/losses/total_variational.py
@@ -6,8 +6,8 @@ class TotalVariationLoss(torch.nn.Module):
         super(TotalVariationLoss, self).__init__()
         self.conv_h = torch.nn.Conv2d(1, 1, kernel_size=(1, 2), stride=1, padding=0, bias=False)
         self.conv_v = torch.nn.Conv2d(1, 1, kernel_size=(2, 1), stride=1, padding=0, bias=False)
-        self.conv_h.weight.data = torch.tensor([[[[1, -1]]]])
-        self.conv_v.weight.data = torch.tensor([[[[1], [-1]]]])
+        self.conv_h.weight.data = torch.tensor([[[[1, -1]]]]).to(torch.float32)
+        self.conv_v.weight.data = torch.tensor([[[[1], [-1]]]]).to(torch.float32)
         for param in self.conv_h.parameters():
             param.requires_grad = False
         for param in self.conv_v.parameters():
@@ -16,10 +16,37 @@ class TotalVariationLoss(torch.nn.Module):
     def forward(self, img):
         device = img.device
         img = img.mean(dim=1, keepdim=True)
-        img_h = self.conv_h(img.to(device))
-        img_v = self.conv_v(img.to(device))
+        img_h = self.conv_h(img.to(torch.float32))
+        img_v = self.conv_v(img.to(torch.float32))
         loss = torch.mean(torch.abs(img_h)) + torch.mean(torch.abs(img_v))
         return loss
+
+
+
+
+
+# import torch
+# import torch.nn.functional as F
+
+# class TotalVariationLoss(torch.nn.Module):
+#     def __init__(self):
+#         super(TotalVariationLoss, self).__init__()
+#         self.conv_h = torch.nn.Conv2d(1, 1, kernel_size=(1, 2), stride=1, padding=0, bias=False)
+#         self.conv_v = torch.nn.Conv2d(1, 1, kernel_size=(2, 1), stride=1, padding=0, bias=False)
+#         self.conv_h.weight.data = torch.tensor([[[[1, -1]]]])
+#         self.conv_v.weight.data = torch.tensor([[[[1], [-1]]]])
+#         for param in self.conv_h.parameters():
+#             param.requires_grad = False
+#         for param in self.conv_v.parameters():
+#             param.requires_grad = False
+
+#     def forward(self, img):
+#         device = img.device
+#         img = img.mean(dim=1, keepdim=True)
+#         img_h = self.conv_h(img.to(device))
+#         img_v = self.conv_v(img.to(device))
+#         loss = torch.mean(torch.abs(img_h)) + torch.mean(torch.abs(img_v))
+#         return loss
 
 
 
