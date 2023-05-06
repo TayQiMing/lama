c7794ec24797b1ab665db0e99a44948ebdbfbf56
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-05-07 01:08:55 +0800

    Update DFCP.py

--- saicinpainting/training/losses/DFCP.py
+++ saicinpainting/training/losses/DFCP.py
@@ -6,50 +6,89 @@ from torchvision import models
 class DFCPLoss(nn.Module):
     def __init__(self):
         super(DFCPLoss, self).__init__()
-        
+
         # Load VGG19 model to extract feature maps
         self.vgg = models.vgg19(pretrained=True).features.eval()
         for param in self.vgg.parameters():
             param.requires_grad = False
-        
-        
+
     def forward(self, predicted, target, mask):
         device = predicted.device
+
         # Extract features from the predicted and target images
         pred_features = self.vgg(predicted)
         target_features = self.vgg(target)
-        
-        # Compute the mean squared error (MSE) between the predicted and target feature maps
-        mse_loss = F.mse_loss(pred_features, target_features)
-        
+
         # Compute the deep feature consistent perceptual loss
-        dfcpl_loss = 0.0
+        fcpl_loss = 0.0
         for pred_feature, target_feature in zip(pred_features, target_features):
-            # Compute the Gram matrix of the predicted and target feature maps
-            pred_gram = self.calc_gram_matrix(pred_feature)
-            target_gram = self.calc_gram_matrix(target_feature)
-            
-            # Compute the MSE between the Gram matrices
-            dfcpl_loss += F.mse_loss(pred_gram, target_gram)
-        
-        # Combine the MSE and deep feature consistent perceptual losses using a weight factor of 0.1
-        loss = mse_loss + 0.1 * dfcpl_loss
-        
+            # Compute the channel-wise L1 distance between the predicted and target feature maps
+            fcpl_loss += F.l1_loss(pred_feature, target_feature)
+
         # Apply the mask to the loss
-        loss = torch.mean(loss * mask.to(device))
+        fcpl_loss = torch.mean(fcpl_loss * mask.to(device))
+
+        return fcpl_loss
+
+
+
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torchvision import models
+
+# class DFCPLoss(nn.Module):
+#     def __init__(self):
+#         super(DFCPLoss, self).__init__()
         
-        return loss
+#         # Load VGG19 model to extract feature maps
+#         self.vgg = models.vgg19(pretrained=True).features.eval()
+#         for param in self.vgg.parameters():
+#             param.requires_grad = False
+        
+        
+#     def forward(self, predicted, target, mask):
+#         device = predicted.device
+#         # Extract features from the predicted and target images
+#         pred_features = self.vgg(predicted)
+#         target_features = self.vgg(target)
+        
+#         # Compute the mean squared error (MSE) between the predicted and target feature maps
+#         mse_loss = F.mse_loss(pred_features, target_features)
+        
+#         # Compute the deep feature consistent perceptual loss
+#         dfcpl_loss = 0.0
+#         for pred_feature, target_feature in zip(pred_features, target_features):
+#             # Compute the Gram matrix of the predicted and target feature maps
+#             pred_gram = self.calc_gram_matrix(pred_feature)
+#             target_gram = self.calc_gram_matrix(target_feature)
+            
+#             # Compute the MSE between the Gram matrices
+#             dfcpl_loss += F.mse_loss(pred_gram, target_gram)
+        
+#         # Combine the MSE and deep feature consistent perceptual losses using a weight factor of 0.1
+#         loss = mse_loss + 0.1 * dfcpl_loss
+        
+#         # Apply the mask to the loss
+#         loss = torch.mean(loss * mask.to(device))
+        
+#         return loss
     
-    def calc_gram_matrix(self, feature_map):
-        batch_size, num_channels, height, width = feature_map.size()
+#     def calc_gram_matrix(self, feature_map):
+# #         batch_size, num_channels, height, width = feature_map.size()
+#         batch_size = feature_map.size(0)
+#         num_channels = feature_map.size(1)
+#         spatial_dim = feature_map.dim() - 2
+#         height = feature_map.size(-2)
+#         width = feature_map.size(-1)
         
-        # Reshape the feature map to a 2D matrix
-        reshaped_feature_map = feature_map.view(batch_size, num_channels, height * width)
+#         # Reshape the feature map to a 2D matrix
+#         reshaped_feature_map = feature_map.view(batch_size, num_channels, height * width)
         
-        # Compute the Gram matrix
-        gram_matrix = torch.bmm(reshaped_feature_map, reshaped_feature_map.transpose(1, 2))
+#         # Compute the Gram matrix
+#         gram_matrix = torch.bmm(reshaped_feature_map, reshaped_feature_map.transpose(1, 2))
         
-        # Normalize the Gram matrix
-        gram_matrix /= (num_channels * height * width)
+#         # Normalize the Gram matrix
+#         gram_matrix /= (num_channels * height * width)
         
-        return gram_matrix
+#         return gram_matrix
