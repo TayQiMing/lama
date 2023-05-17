9b83c91dc72a1e8eff129bfa463ad7d6a23ac535
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-05-17 18:37:51 +0800

    Update HAD.py

--- saicinpainting/training/losses/HAD.py
+++ saicinpainting/training/losses/HAD.py
@@ -50,8 +50,8 @@ class HADLoss(torch.nn.Module):
 
     def forward(self,feature_i,feature_g, img, predicted_img,discrim, ori_mask, supervised_mask):
         device = predicted_img.device
-        print(len(feature_i[0][0]))
-        print("AND " , len(feature_g[0][0]))
+        print(len(feature_i[0][0][0]),len(feature_i[0][0][0][0]),len(feature_i[0][0][0][0]))
+        print("AND " , len(feature_g[0][0][0]),len(feature_g[0][0][0][0]),len(feature_g[0][0][0][0]))
         # Compute features for inpainted and ground truth images
 #         _, feat_i = discrim(predicted_img)
 #         _, feat_g = discrim(img)
