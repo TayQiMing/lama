58798447e8ca196c6ca7f171f6400da53d9c2851
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-05-17 21:29:08 +0800

    Update AA.py

--- saicinpainting/training/losses/AA.py
+++ saicinpainting/training/losses/AA.py
@@ -1,38 +1,110 @@
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+
+# class AttentionalAdversarialLoss(nn.Module):
+#     def __init__(self):
+#         super(AttentionalAdversarialLoss, self).__init__()
+        
+
+#     def forward(self,discriminator, generator_output, target_image, attention_mask):
+#         # Generate fake images
+#         fake_images = generator_output
+
+#         # Discriminate real and fake images
+#         real_preds = discriminator(target_image)
+#         fake_preds = discriminator(fake_images)[0]
+
+#         # Calculate adversarial loss
+#         adversarial_loss = -torch.mean(torch.log(fake_preds))
+
+#         # Apply attention mask to guide the generation process
+#         guided_fake_images = fake_images * attention_mask
+#         guided_target_image = target_image * attention_mask
+
+#         # Calculate attentional loss
+#         attentional_loss = F.mse_loss(guided_fake_images, guided_target_image)
+
+#         # Calculate the total loss
+#         total_loss = adversarial_loss + attentional_loss
+
+#         return total_loss
+
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
+
 
 class AttentionalAdversarialLoss(nn.Module):
     def __init__(self):
         super(AttentionalAdversarialLoss, self).__init__()
         
+        # Load VGG19 model to extract feature maps
+        self.vgg = models.vgg19(pretrained=True).features.eval()
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+        
+        # Define the adversarial loss criterion
+        self.criterion = nn.BCEWithLogitsLoss()
+        
+        
+    def forward(self, generator, discriminator, predicted_img, real_img, mask):
+        device = predicted_img.device
+        
+        # Generate a fake image by filling in the masked region of the real image
+        fake_img = predicted_img * mask + real_img * (1 - mask)
+        
+        # Compute the attention maps for the real and fake images
+        real_atts = self.calc_attention_maps(real_img)
+        fake_atts = self.calc_attention_maps(fake_img)
+        
+        # Compute the feature maps for the real and fake images
+        real_features = self.vgg(real_img)
+        fake_features = self.vgg(fake_img)
+        
+        # Compute the adversarial loss for the fake image
+        fake_preds = discriminator(fake_img)
+        adv_loss = self.criterion(fake_preds, torch.ones_like(fake_preds).to(device))
+        
+        # Compute the attentional loss
+        att_loss = F.l1_loss(real_atts * fake_features, fake_atts * real_features)
+        
+        # Combine the adversarial and attentional losses using a weight factor of 0.001
+        loss = adv_loss + 0.001 * att_loss
+        
+        return loss
+    
+    def calc_attention_maps(self, img):
+        # Compute the gradients of the image with respect to the pixel values
+        img_grads = torch.abs(torch.autograd.grad(outputs=img.sum(), inputs=img, create_graph=True)[0])
 
-    def forward(self,discriminator, generator_output, target_image, attention_mask):
-        # Generate fake images
-        fake_images = generator_output
+        # Compute the attention maps
+        att_maps = torch.mean(img_grads, dim=1, keepdim=True)
+        att_maps = F.interpolate(att_maps, size=img.shape[2:], mode='bilinear', align_corners=False)
+        att_maps = F.normalize(att_maps, p=1, dim=2)
 
-        # Discriminate real and fake images
-        real_preds = discriminator(target_image)
-        fake_preds = discriminator(fake_images)[0]
-
-        # Calculate adversarial loss
-        adversarial_loss = -torch.mean(torch.log(fake_preds))
-
-        # Apply attention mask to guide the generation process
-        guided_fake_images = fake_images * attention_mask
-        guided_target_image = target_image * attention_mask
-
-        # Calculate attentional loss
-        attentional_loss = F.mse_loss(guided_fake_images, guided_target_image)
-
-        # Calculate the total loss
-        total_loss = adversarial_loss + attentional_loss
-
-        return total_loss
+        return att_maps
 
 
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 # import torch
 # import torch.nn as nn
