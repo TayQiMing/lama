a1eb4327d8ef99e1f58c16f81db5b6255c903070
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-05-19 17:30:01 +0800

    Update train.py

--- bin/train.py
+++ bin/train.py
@@ -61,6 +61,7 @@ def main(config: OmegaConf):
             default_root_dir=os.getcwd(),
             **trainer_kwargs
         )
+        print("######## MY TRAINER INFO ########",trainer)
         trainer.fit(training_model)
 #         print("@@@@@ FIT TRAINING MODEL HERE @@@@@")
     except KeyboardInterrupt:
